86c0a762d794d9750283f19a6596073286b6959b
Martin <skylinecho@abv.bg>
2025-02-11 22:17:31 +0200

    Adding MonthsCode Task

---
+++ 15_MonthsCode.js
@@ -0,0 +1,57 @@
+function solve(num){
+    if (num == 1){
+        console.log("January")
+    }
+    else if (num == 2){
+        console.log("February")
+    }
+    else if (num == 3){
+        console.log("March")
+    }
+    else if (num == 4){
+        console.log("April")
+    }
+    else if (num == 5){
+        console.log("May")
+    }
+    else if (num == 6){
+        console.log("June")
+    }
+    else if (num == 7){
+        console.log("July")
+    }
+    else if (num == 8){
+        console.log("August")
+    }
+    else if (num == 9){
+        console.log("September")
+    }
+    else if (num == 10){
+        console.log("October")
+    }
+    else if (num == 11){
+        console.log("November")
+    }
+    else if (num == 12){
+        console.log("December")
+    }
+    else num <1 >12,
+        console.log("Error!")    
+}
+
+solve(1)
+solve(2)
+solve(3)
+solve(4)
+solve(5)
+solve(6)
+solve(7)
+solve(8)
+solve(9)
+solve(10)
+solve(11)
+solve(12)
+solve(0)
+solve(13)
+solve(-259)
+solve(122)
