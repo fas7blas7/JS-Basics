c0030b77d6061847e3dbf22f3098aed25c6831b4
Martin <skylinecho@abv.bg>
2025-02-11 22:11:11 +0200

    Adding StudentInformation Task

---
+++ 13.StudentInformation.js
@@ -0,0 +1,48 @@
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade:` ,grade.toFixed(2));
+    }
+
+solve('John', 15, 5.54678);
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade:` ,grade.toFixed(2));
+    }
+
+solve('Steve', 16, 2.1426);
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade:` ,grade.toFixed(2));
+    }
+
+solve('Marry', 12, 6.00);
+
+
+//Example with both grade with decimal point beyond the second one
+
+
+function studentFullData(name, age, grade){
+    console.log(`The name is ${name}, age is ${age} and the grade is ${grade}`, `The rounded up grade is of the student is`, grade.toFixed(2));
+    }
+
+studentFullData('John', 15, 5.54678);
+
+
+///////////////////////////
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age} Grade: ${grade}`);
+    }
+
+solve('John', 15, 5.54678);
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age} Grade: ${grade}`);
+    }
+
+solve('Steve', 16, 2.1426);
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age} Grade: ${grade}`);
+    }
+
+solve('Marry', 12, 6.00);
