ab132799a1a7dd95f57d9adb92956b2bf09e3bba
Martin <skylinecho@abv.bg>
2025-02-12 10:51:24 +0200

    Adding SumFirstAndLastElementInArray task

---
+++ 21_SumFirstAndLastELEMENTInArray.js
@@ -0,0 +1,8 @@
+function solve(input){
+    let firstNumber = Number(input[0]);
+    let lastNumber = Number(input[input.length -1]);
+
+    console.log(firstNumber + lastNumber)
+}
+
+solve([10, 20, 30, 40, 50, 60, 70, 80, 90])
