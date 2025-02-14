060b5702384f1962c10428ff41ccdd5eeb3e0757
Martin <skylinecho@abv.bg>
2025-02-14 21:41:21 +0200

    Adding PersonInfoObject task

---
+++ 31_PersonInfoObject.js
@@ -0,0 +1,12 @@
+function person(firstName, lastName, age)
+{
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+    
+    return person;   
+}
+
+console.log(person("Djeki", "Djekov", 15))
